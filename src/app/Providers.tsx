6fe2b6dbfd4044123fa19481a70f8d749a469e6f
poylib <poy_lib@naver.com>
2024-03-21 22:35:46 +0900

    Test: deploy test

---
+++ src/app/Providers.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function Providers({ children }: React.PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000 * 10,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main>
+      <span>/on-the-desk</span>
+      <span>deploy test</span>
+    </main>
+  );
+}
