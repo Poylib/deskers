61539e0dc1fa110c8ca819ccb83366cd01774118
poylib <poy_lib@naver.com>
2024-04-04 00:15:55 +0900

    Feature: 메인 ui 작업중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <main>
-      <span>deploy test1</span>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.category}>
+          <span>전체 (4)</span>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.thumbnail} />
+        </div>
+      </div>
     </main>
   );
 }
