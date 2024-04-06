fd7fb38dd7517954634e1d91ac3252835d0df0dc
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-06 22:45:21 +0900

    기존 코드를 제거합니다

--- src/app/components/navigation/sideBar/SideBar.tsx
+++ src/app/components/navigation/sideBar/SideBar.tsx
@@ -1,24 +1,19 @@
 import Link from 'next/link';
-import { getCategoryList } from '@/api/category';
 
 import styles from './SideBar.module.scss';
 
 export default function SideBar() {
-  const categoryList = getCategoryList();
-  const sortedCategoryList: any[] = Object.values(
-    Object.fromEntries(Object.entries(categoryList.categoryDict).sort(([, categoryA], [, categoryB]) => categoryA.depth - categoryB.depth))
-  );
   return (
     <aside className={styles.sideBar}>
       <ul>
-        {sortedCategoryList.map((v, index) => {
+        {/* {sortedCategoryList.map((v, index) => {
           // console.log('v :', v);
           return (
             <li key={index} style={{ paddingLeft: `${v.depth * 10}px` }}>
               <Link href={`/${v.uri}`}>{v.name}</Link>
             </li>
           );
-        })}
+        })} */}
       </ul>
     </aside>
   );
