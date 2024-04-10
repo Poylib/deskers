fa2e0d6898d4c6a259f7fbf19084956d5505207e
poylib <poy_lib@naver.com>
2024-04-11 00:18:14 +0900

    Feature: add content detail ui

--- src/app/[category]/[...slug]/components/Thumbnail.tsx
+++ src/app/[category]/[...slug]/components/Thumbnail.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import test from '@@/images/test.jpg';
 export default function Thumbnail() {
   return (
-    <div>
+    <div style={{ width: '100%' }}>
       <Image src={test} style={{ objectFit: 'cover', height: 240, width: '100%' }} alt={`컨텐츠 썸네일`} />
     </div>
   );
