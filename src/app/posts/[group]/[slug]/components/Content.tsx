01c5fdebb8c8741e36a8113ecc1a0cfa8dfafd8a
poylib <poy_lib@naver.com>
2024-04-12 23:58:15 +0900

    Feature: 상세페이지 마크다운 펼치는 작업중

--- src/app/posts/[group]/[slug]/components/Content.tsx
+++ src/app/posts/[group]/[slug]/components/Content.tsx
@@ -1,3 +1,21 @@
-export default function Content() {
-  return <span>content</span>;
+'use client';
+
+import { remark } from 'remark';
+import html from 'remark-html';
+import styles from './Content.module.scss';
+import { useEffect, useState } from 'react';
+
+export default function Content({ post }: { post: Post }) {
+  const [htmlContent, setHtmlContent] = useState<string>('');
+
+  useEffect(() => {
+    const fetchHtmlContent = async () => {
+      const result = await remark().use(html).process(post.content);
+      setHtmlContent(result.toString());
+    };
+
+    fetchHtmlContent();
+  }, [post.content]);
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
