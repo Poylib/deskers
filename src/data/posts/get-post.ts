727cb77e72dadd3a6d7514bc912a03bc8b93cfd5
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-13 15:44:40 +0900

    [페이지 경로] 슬러그 방식으로 페이지 경로를 수정합니다

--- src/data/posts/get-post.ts
+++ src/data/posts/get-post.ts
@@ -8,11 +8,12 @@ import { Post, PostMatter } from '../model/type';
 export const getPost = async (filepath: string): Promise<Post> => {
   const mdx = matter(fs.readFileSync(filepath, 'utf8'));
   const grayMatter = mdx.data as PostMatter & { category: string };
+  const parsed = path.parse(filepath);
   return {
     ...grayMatter,
     dateString: dayjs(grayMatter.date).locale('ko').format('YYYY년 MM월 DD일'),
     content: mdx.content,
     readingMinutes: Math.ceil(readingTime(mdx.content).minutes),
-    filepath: path.relative(process.cwd(), filepath),
+    uri: path.relative(process.cwd(), path.join(parsed.dir.replace('posts/', ''), parsed.name)),
   };
 };
