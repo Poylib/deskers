a3f85306ca5d3392b949614d7b58daaf5e29b4e9
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-13 15:23:03 +0900

    [리팩토링] 불필요한 분기를 제거합니다

--- src/app/[group]/page.tsx
+++ src/app/[group]/page.tsx
@@ -9,21 +9,10 @@ import { Group } from '@/data/model/type';
 export default async function Category({ params, searchParams }: any) {
   const { group: _group } = params;
   const group = getGroups().find((group) => group.category.includes(_group))! as Group;
-  if (searchParams.category) {
-    const posts = await getPosts({
-      group: _group,
-      category: searchParams.category,
-    });
-    return (
-      <main className={styles.main}>
-        <Title group={group} />
-        <ContentList posts={posts} />
-      </main>
-    );
-  }
   const posts = await getPosts({
-    group: _group,
-  });
+    group: group.category,
+    category: searchParams.category,
+  })
   return (
     <main className={styles.main}>
       <Title group={group} />
