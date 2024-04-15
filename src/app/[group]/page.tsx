34dc065763a9d5a36cfc0493e8e89478d138db62
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-15 19:01:18 +0900

    [헤더 / 네비게이션] 반응형 메뉴로 변경합니다

--- src/app/[group]/page.tsx
+++ src/app/[group]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import styles from './page.module.scss';
-import { getGroups, getPosts } from '@/data/posts';
+import { getPosts } from '@/data/posts';
 import Title from '@/lib/components/group/list/Title';
 import ContentList from '@/lib/components/group/list/ContentList';
 import { Group } from '@/data/model/type';
+import { getGroups } from '@/data/posts/get-groups';
 
 export default async function Category({ params, searchParams }: any) {
   const { group: _group } = params;
---
+++ src/data/posts/get-groups.ts
@@ -0,0 +1,19 @@
+import { sync } from 'glob';
+import path from 'path';
+import fs from 'fs';
+import { Group } from '../model/type';
+import { POSTS_PATH } from '@/config';
+
+export function getGroups(): Group[] {
+  return sync('posts/*', { absolute: false })
+    .map((filepath) => path.basename(filepath))
+    .map((category) => {
+      const displayName = fs.readFileSync(path.join(POSTS_PATH, category, '_meta'), { encoding: 'utf-8' });
+      const glob = sync(`${path.join(POSTS_PATH, category) || '**'}/**/*.mdx`).length;
+      return {
+        category,
+        displayName,
+        count: glob,
+      };
+    });
+}
