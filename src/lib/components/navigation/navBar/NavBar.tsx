9426ecb5470e19febcda91bc5beb42e1a3613f4b
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-15 19:01:18 +0900

    [헤더 / 네비게이션] 반응형 메뉴로 변경합니다

--- src/lib/components/navigation/navBar/NavBar.tsx
+++ src/lib/components/navigation/navBar/NavBar.tsx
@@ -1,25 +1,175 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Group } from '@/data/model/type';
 import Link from 'next/link';
 
-import styles from './NavBar.module.scss';
-import { getGroups } from '@/data/posts';
-
-export default async function NavBar() {
-  const groups = getGroups();
-  return (
-    <header className={styles.container}>
-      <Link href={'/'}>
-        <span>Deskers</span>
-      </Link>
-      <div className={styles.category}>
-        <Link href={`/about`}>
-          <span>ABOUT</span>
-        </Link>
-        {groups.map((el, idx) => (
-          <Link key={`${idx}`} href={`/${el.category}`}>
-            <span>{el.displayName}</span>
-          </Link>
-        ))}
-      </div>
-    </header>
-  );
+interface HeaderProps {
+  groups: Group[];
+  toggleNav: () => void;
+  active: boolean;
 }
+
+const StyledRootDiv = styled.div`
+  :root {
+    --main-color: #5d9ab2;
+    --accent-color: #bf6a7a;
+    --dark-main-color: #2b5566;
+    --text-bright-color: #fff;
+    --icon-color: #fff;
+    --icon-bk-color: #ddd;
+    --large-width: 1000px;
+  }
+
+  body {
+    margin: 0;
+    font-family: '맑은 고딕', 'Apple SD Gothic Neo', sans-serif;
+  }
+
+  header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+
+  .headA {
+    display: inline-block;
+    line-height: 70px;
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: #5d9ab2;
+    background-color: var(--main-color);
+    color: #fff;
+    color: var(--text-bright-color);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+    text-decoration: none;
+  }
+
+  @media (min-width: 768px) {
+    & {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      max-width: 1000px;
+      max-width: var(--large-width);
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .headB ul {
+      display: flex;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .container-small {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .headC {
+      margin-right: 10px;
+      padding: 0;
+      border: none;
+      outline: none;
+      background: none;
+      font-size: 28px;
+      opacity: 0.5;
+      cursor: pointer;
+    }
+
+    .headC:hover {
+      opacity: 0.3;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .headC {
+      display: none;
+    }
+  }
+`;
+
+const StyledNavWrapper = styled.div`
+  .headB ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .headB a {
+    display: block;
+    padding: 15px;
+    color: inherit;
+    font-size: 12px;
+    text-decoration: none;
+  }
+
+  .headB a:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  @media (max-width: 767px) {
+    .headB {
+      display: none;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .headB {
+      display: block !important;
+    }
+  }
+  ${({ active }: any) =>
+    active &&
+    `
+    .headB {
+      display: block !important;
+    }
+  `}
+` as any;
+
+const NavBar: React.FC<HeaderProps> = ({ groups, active, toggleNav }) => {
+  return (
+    <>
+      <StyledRootDiv>
+        <div className="container-small">
+          <a href="index.html" className="headA">
+            Deskers
+          </a>
+          <button type="button" className="headC" onClick={toggleNav}>
+            <FontAwesomeIcon icon={faBars} title="MENU" />
+          </button>
+        </div>
+        <StyledNavWrapper active={active}>
+          <nav className="headB">
+            <ul>
+              <li>
+                <Link href={`/about`}>
+                  <span>ABOUT</span>
+                </Link>
+              </li>
+
+              {groups?.map((group, idx) => {
+                return (
+                  <li key={idx}>
+                    <a href={group.category}>{group.displayName}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </StyledNavWrapper>
+      </StyledRootDiv>
+    </>
+  );
+};
+
+export default NavBar;
