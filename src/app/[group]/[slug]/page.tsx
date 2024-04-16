2f828edbacd6bb6b5501806a9608a379488e5892
poylib <poy_lib@naver.com>
2024-04-16 23:29:49 +0900

    Feature: styled layout 추가중

--- src/app/[group]/[slug]/page.tsx
+++ src/app/[group]/[slug]/page.tsx
@@ -1,21 +1,21 @@
-import styles from './page.module.scss';
 import Content from '@/lib/components/detail/Content';
 import ContentHeader from '@/lib/components/detail/ContentHeader';
 import { getPostBySlug } from '@/data/posts';
 import Thumbnail from '@/lib/components/detail/Thumbnail';
+import Container from '@/core/layout/Container';
 
 const Page = async ({ params }: any) => {
   const { group, slug } = params;
 
   const post = await getPostBySlug({ group, slug });
   return (
-    <main className={styles.container}>
+    <Container>
       <div style={{ maxWidth: 900, margin: 'auto', width: '100%' }}>
         <Thumbnail img={post.thumbnail} />
         <ContentHeader post={post} />
         <Content post={post} />
       </div>
-    </main>
+    </Container>
   );
 };
 
