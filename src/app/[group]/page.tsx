030bab006747712e8cdab6f01a9b6ee7c62a4b4e
poylib <poy_lib@naver.com>
2024-04-16 23:29:49 +0900

    Feature: styled layout 추가중

--- src/app/[group]/page.tsx
+++ src/app/[group]/page.tsx
@@ -5,20 +5,20 @@ import Title from '@/lib/components/group/list/Title';
 import ContentList from '@/lib/components/group/list/ContentList';
 import { Group } from '@/data/model/type';
 import { getGroups } from '@/data/posts/get-groups';
+import Container from '@/core/layout/Container';
 
 export default async function Category({ params, searchParams }: any) {
   const { group: _group } = params;
   const group = getGroups().find((group) => group.category.includes(_group))! as Group;
-  console.log('group : undefined', group);
-  if (!group) return;
+
   const posts = await getPosts({
     group: group.category,
     category: searchParams.category,
   });
   return (
-    <main>
+    <Container>
       <Title group={group} />
       <ContentList posts={posts} />
-    </main>
+    </Container>
   );
 }
