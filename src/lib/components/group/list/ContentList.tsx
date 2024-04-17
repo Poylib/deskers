1171f33f72f31952ee8362bcf8b97b7dae86d061
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-17 20:50:34 +0900

    [UI 개선 / 컨텐츠 목록 페이지] 반응형 처리

--- src/lib/components/group/list/ContentList.tsx
+++ src/lib/components/group/list/ContentList.tsx
@@ -1,21 +1,33 @@
+'use client';
 import { ImageList } from '@mui/material';
 import Content from '../hash-list/Content';
 
-import styles from './ContentList.module.scss';
 import { Post } from '@/data/model/type';
+import styled from 'styled-components';
 
 type Props = {
   posts: Post[];
 };
 
+const StyledRootDiv = styled.div`
+  .container {
+    min-width: 200px;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
 export default function ContentList({ posts }: Props) {
   return (
-    <section className={styles.container}>
-      <ImageList sx={{ height: '100%', gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr)) !important` }} gap={12}>
-        {posts.map((item, idx) => (
-          <Content key={`${idx}`} item={item} />
-        ))}
-      </ImageList>
-    </section>
+    <StyledRootDiv>
+      <section className="container">
+        <ImageList sx={{ height: '100%', gridTemplateColumns: `repeat(auto-fill, minmax(200px, 1fr)) !important` }} gap={12}>
+          {posts.map((item, idx) => (
+            <Content key={`${idx}`} item={item} />
+          ))}
+        </ImageList>
+      </section>
+    </StyledRootDiv>
   );
 }
