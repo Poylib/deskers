c33a7ea9d32f792ef236582cc82fa6d788c8a635
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-17 10:58:35 +0900

    [UI 개선 / 컨텐츠 상세 페이지] 반응형 처리

--- src/lib/components/detail/Content.tsx
+++ src/lib/components/detail/Content.tsx
@@ -1,10 +1,34 @@
+'use client';
 import 'github-markdown-css';
 import { Post } from '@/data/model/type';
+import styled from 'styled-components';
+
+const StyledRootDiv = styled.div`
+  width: 1000px;
+  min-width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media (max-width: 768px) {
+    width: 600px;
+    min-width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media (max-width: 767px) {
+    width: 80%;
+    min-width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
 
 export default function Content({ post }: { post: Post }) {
   return (
-    <div className={'markdown-body'} style={{ paddingTop: 40, paddingBottom: 40 }}>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-    </div>
+    <StyledRootDiv>
+      <div className={'markdown-body'} style={{ paddingTop: 40, paddingBottom: 40 }}>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </StyledRootDiv>
   );
 }
