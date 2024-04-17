4d6fd43cde4f8bc03d199b6d6f803b081baa92cf
poylib <poy_lib@naver.com>
2024-04-17 23:48:16 +0900

    Feature: content 상세 ui

--- src/lib/components/detail/Content.tsx
+++ src/lib/components/detail/Content.tsx
@@ -4,31 +4,53 @@ import { Post } from '@/data/model/type';
 import styled from 'styled-components';
 
 const StyledRootDiv = styled.div`
-  width: 1000px;
-  min-width: 400px;
-  margin-left: auto;
-  margin-right: auto;
+  min-width: 320px;
+  padding: 0 24px;
 
-  @media (max-width: 768px) {
-    width: 600px;
-    min-width: 400px;
-    margin-left: auto;
-    margin-right: auto;
+  letter-spacing: -0.004em;
+  word-break: keep-all;
+  overflow-wrap: break-word;
+
+  h2 {
+    margin: 2.5rem 0 1rem;
+    line-height: 1.5;
+    font-size: 2rem;
   }
-  @media (max-width: 767px) {
-    width: 80%;
-    min-width: 200px;
-    margin-left: auto;
-    margin-right: auto;
+
+  h3 {
+    margin: 2rem 0 1rem;
+    line-height: 1.4;
+    font-size: 1.5rem;
+  }
+
+  h4 {
+    margin: 1.5rem 0 0.5rem;
+    line-height: 1.3;
+    font-size: 1.25rem;
+  }
+
+  h5 {
+    margin: 1rem 0 0.25rem;
+    line-height: 1.2;
+    font-size: 1.125rem;
+  }
+
+  p {
+    line-height: 1.7;
+    font-size: 1.125rem;
+  }
+
+  ul,
+  ol {
+    line-height: 1.7;
+    font-size: 1.125rem;
   }
 `;
 
 export default function Content({ post }: { post: Post }) {
   return (
     <StyledRootDiv>
-      <div className={'markdown-body'} style={{ paddingTop: 40, paddingBottom: 40 }}>
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </StyledRootDiv>
   );
 }
--- src/lib/components/detail/ContentHeader.tsx
+++ src/lib/components/detail/ContentHeader.tsx
@@ -1,18 +1,46 @@
+'use client';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
-import styles from './ContentHeader.module.scss';
 import { Post } from '@/data/model/type';
+import styled from 'styled-components';
 
 export default function ContentHeader({ post }: { post: Post }) {
   return (
-    <div className={styles.container}>
-      <span className={styles.categoryName}>{post.category}</span>
+    <StyledRootDiv>
+      <span className="categoryName">{post.category}</span>
       <h1>{post.title}</h1>
-      <div className={styles.date}>
+      <div className="date">
         <CalendarTodayIcon sx={{ fontSize: 20 }} />
         <span>{post.dateString}</span>
       </div>
       <span>읽는 시간 {post.readingMinutes} 분</span>
-    </div>
+    </StyledRootDiv>
   );
 }
+
+const StyledRootDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 900px;
+  padding: 24px;
+
+  .categoryName {
+    margin-bottom: 24px;
+  }
+
+  h1 {
+    font-size: 2rem;
+    line-height: 1.5;
+  }
+
+  .date {
+    display: flex;
+    align-items: center;
+    margin: 24px 0;
+
+    span {
+      margin-left: 8px;
+    }
+  }
+`;
