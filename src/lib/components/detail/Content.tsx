142afaa02e0dfe929ca0290b35972f855005a681
portfolio-y0711 <portfolio.y0711@gmail.com>
2024-04-17 20:38:18 +0900

    [마크다운 컨버터] 기존 마크다운 렌더링 컴포넌트를 제거합니다

--- src/lib/components/detail/Content.tsx
+++ src/lib/components/detail/Content.tsx
@@ -1,11 +1,10 @@
-import { MDXRemote } from 'next-mdx-remote/rsc';
 import 'github-markdown-css';
 import { Post } from '@/data/model/type';
 
 export default function Content({ post }: { post: Post }) {
   return (
     <div className={'markdown-body'} style={{ paddingTop: 40, paddingBottom: 40 }}>
-      <MDXRemote source={post.content} />
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </div>
   );
 }
