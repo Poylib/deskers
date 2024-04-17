91abdef0dae98af3f17b4ae3cfc7403013ed05a7
poylib <poy_lib@naver.com>
2024-04-17 23:48:16 +0900

    Feature: content 상세 ui

--- src/lib/components/detail/Thumbnail.tsx
+++ src/lib/components/detail/Thumbnail.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Thumbnail({ img }: { img: string }) {
   if (!img) return;
   return (
-    <div style={{ width: '100%' }}>
-      <Image src={img} style={{ objectFit: 'cover' }} alt={`컨텐츠 썸네일`} width={900} height={300} />
+    <div style={{ width: '100%', height: 300, position: 'relative' }}>
+      <Image src={img} style={{ objectFit: 'contain' }} layout="fill" alt={`컨텐츠 썸네일`} />
     </div>
   );
 }
