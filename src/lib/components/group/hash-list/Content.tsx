6baaa797dc18bfbd3b2a6c96b4b0edab23aa5474
GyuTae Kim <supxyrho@gmail.com>
2024-04-19 13:56:38 +0900

    feat(hash-list/Content): subTitle 내 MDX 문법을 제거한 문자열만 출력하도록 가공한다.

--- src/lib/components/group/hash-list/Content.tsx
+++ src/lib/components/group/hash-list/Content.tsx
@@ -12,7 +12,7 @@ export default function Content({ item }: Props) {
   return (
     <Link href={`/${item?.uri}`}>
       <ImageListItem style={{ cursor: 'pointer' }} >
-        <ImageListItemBar title={item.title} subtitle={<span>{item.content}</span>}/>
+        <ImageListItemBar title={item.title} subtitle={<span>{removeMDXSyntax(item.content)}</span>}/>
           <Stack direction={'row'}>
             <Image
               src={item.thumbnail}
@@ -26,4 +26,34 @@ export default function Content({ item }: Props) {
       </ImageListItem>
     </Link>
   );
+}
+
+// @TODO: 추후 외부 파일로 분리
+function removeMDXSyntax(str: string) {
+  // 굵은 텍스트, 이탤릭 텍스트 제거
+  str = str.replace(/\*\*(.*?)\*\*/g, '$1'); // 굵은 텍스트
+  str = str.replace(/\*(.*?)\*/g, '$1');     // 이탤릭 텍스트
+
+  // 링크 제거 - 링크 텍스트만 남기고 URL 제거
+  str = str.replace(/\[([^\]]+)\]\([^\)]+\)/g, '$1');
+
+  // 인라인 코드 제거
+  str = str.replace(/`([^`]+)`/g, '$1');
+
+  // 코드 블록 제거
+  str = str.replace(/```[\s\S]*?```/g, '');
+
+  // 헤더 제거
+  str = str.replace(/#+\s*(.*)/g, '$1');
+
+  // 이미지 제거
+  str = str.replace(/!\[[^\]]*\]\([^)]*\)/g, '');
+
+  // <br /> 제거
+  str = str.replace(/<br\s*\/?>/gi, '')
+
+  // 리스트 아이템 제거 ('-' 또는 '*'로 시작)
+  str = str.replace(/^\s*[\-\*]\s+(.*)/gm, '$1');
+
+  return str;
 }
