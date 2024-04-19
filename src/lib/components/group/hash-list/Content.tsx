6a93feb8e44a3e055c1925f1e967c9de0437bfe9
GyuTae Kim <supxyrho@gmail.com>
2024-04-19 13:55:20 +0900

    feat(hash-list/Content.tsx): 콘텐츠 목록 내 아이템의 레이아웃 변경

--- src/lib/components/group/hash-list/Content.tsx
+++ src/lib/components/group/hash-list/Content.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image';
-import { ImageListItem, ImageListItemBar } from '@mui/material';
+import { ImageListItem, ImageListItemBar, Stack } from '@mui/material';
 import Link from 'next/link';
 import { Post } from '@/data/model/type';
 
@@ -11,16 +11,18 @@ type Props = {
 export default function Content({ item }: Props) {
   return (
     <Link href={`/${item?.uri}`}>
-      <ImageListItem style={{ cursor: 'pointer' }} onClick={() => {}}>
-        <Image
-          src={item.thumbnail}
-          alt={item.thumbnail}
-          loading="eager"
-          width={300}
-          height={300}
-          style={{ objectFit: 'cover', width: '100%', height: 300 }}
-        />
-        <ImageListItemBar title={item.title} subtitle={<span>{item.content}</span>} position="below" />
+      <ImageListItem style={{ cursor: 'pointer' }} >
+        <ImageListItemBar title={item.title} subtitle={<span>{item.content}</span>}/>
+          <Stack direction={'row'}>
+            <Image
+              src={item.thumbnail}
+              alt={item.thumbnail}
+              loading="eager"
+              width={300}
+              height={300}
+              style={{ objectFit: 'cover', width: '100%', height: 300 }}
+            />
+          </Stack>
       </ImageListItem>
     </Link>
   );
