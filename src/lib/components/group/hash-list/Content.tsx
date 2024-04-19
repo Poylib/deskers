b7c4d217022d1eae3a51c78b2fedc7f0339d5a48
GyuTae Kim <supxyrho@gmail.com>
2024-04-19 14:26:31 +0900

    fix(hash-list/Content.tsx): 불필요한 컴포넌트 제거

--- src/lib/components/group/hash-list/Content.tsx
+++ src/lib/components/group/hash-list/Content.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image';
-import { ImageListItem, ImageListItemBar, Stack } from '@mui/material';
+import { ImageListItem, ImageListItemBar } from '@mui/material';
 import Link from 'next/link';
 import { Post } from '@/data/model/type';
 
@@ -13,16 +13,14 @@ export default function Content({ item }: Props) {
     <Link href={`/${item?.uri}`}>
       <ImageListItem style={{ cursor: 'pointer' }} >
         <ImageListItemBar title={item.title} subtitle={<span>{removeMDXSyntax(item.content)}</span>}/>
-          <Stack direction={'row'}>
-            <Image
-              src={item.thumbnail}
-              alt={item.thumbnail}
-              loading="eager"
-              width={300}
-              height={300}
-              style={{ objectFit: 'cover', width: '100%', height: 300 }}
-            />
-          </Stack>
+          <Image
+            src={item.thumbnail}
+            alt={item.thumbnail}
+            loading="eager"
+            width={300}
+            height={300}
+            style={{ objectFit: 'cover', width: '100%', height: 300 }}
+          />
       </ImageListItem>
     </Link>
   );
