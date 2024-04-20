41cc58026d3ac69e66b724954bf06893a94fffb5
poylib <poy_lib@naver.com>
2024-04-20 09:58:01 +0900

    Fix: meta 누락 해결

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,20 +1,16 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
-import { useSpyElem } from "../hook/useSpy";
-import ThemeSwitch from "./theme/Switch";
-import ScrollProgressBar from "../components/ScrollProgressBar";
+import { useSpyElem } from '../hook/useSpy';
+import ThemeSwitch from './theme/Switch';
+import ScrollProgressBar from '../components/ScrollProgressBar';
 
 export const Header = () => {
   const { ref, marginTop } = useSpyElem(65);
 
   return (
-    <nav
-      style={{ marginTop }}
-      ref={ref}
-      className="fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm"
-    >
+    <nav style={{ marginTop }} ref={ref} className="fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm">
       <div className="mt-1 flex h-[64px] w-full max-w-[1200px] items-center justify-between px-4">
         <div className="flex items-center text-lg font-medium">
           <Link href="/">DESKERS</Link>
@@ -25,6 +21,9 @@ export const Header = () => {
           </div>
           <div className="flex items-center text-sm font-medium">
             <Link href="/content-seo">CONTENTS-SEO</Link>
+          </div>
+          <div className="flex items-center text-sm font-medium">
+            <Link href="/deskterior">테스크테리어</Link>
           </div>
           <ThemeSwitch />
         </div>
