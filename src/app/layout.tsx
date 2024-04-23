8505ce6d01063e7d83bbe704b4fa4a412a3dd4f1
poylib <poy_lib@naver.com>
2024-04-23 13:58:01 +0900

    Feature: clarity setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { Toaster } from '../sections/ui/toaster';
 import { Header } from '../layouts/Header';
 import { Footer } from '../layouts/Footer';
 import GoogleAnalytics from './lib/GoogleAnalytics';
+import MicrosoftClarity from './lib/MicrosoftClarity';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -42,7 +43,12 @@ export default function RootLayout({
   return (
     <html lang="ko" className="h-full scroll-my-20 scroll-smooth" suppressHydrationWarning>
       <body className={cn(inter.className, 'flex min-h-screen flex-col')}>
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} /> : null}
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <>
+            <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+            <MicrosoftClarity />
+          </>
+        ) : null}
         <ThemeProvider>
           <Header />
           <main className="mt-[64px] flex flex-1 flex-col">{children}</main>
